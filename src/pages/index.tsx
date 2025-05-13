3fb310649a4612d2e427a3ea39cde443dea7b81d
Kartik989-max <bhatnagarkartik6.com>
2025-05-13 14:23:30 +0530

    main

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Categories from "@/components/Categories";
-import FeatureSection from "@/components/FeatureCards";
 import HeroSection from "@/components/HeroSection"
 import Navbar from "@/components/Navbar"
 import RitualKits from "@/components/RitualKits";
