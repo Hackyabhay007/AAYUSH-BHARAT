2fbc9fa92a73073a5e58378f8023d1c0fb1971e3
Kartik989-max <bhatnagarkartik6.com>
2025-05-19 14:22:20 +0530

    main

--- src/app/(pages)/shop/components/ProductGrid.tsx
+++ src/app/(pages)/shop/components/ProductGrid.tsx
@@ -5,7 +5,6 @@ import { Product } from "@/appwrite/product";
 interface ProductGridProps {
   products: Product[];
 }
-
 const ProductGrid: React.FC<ProductGridProps> = ({ products }) => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-6 px-4">
